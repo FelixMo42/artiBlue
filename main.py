96b93b5531e790a6ee5413017604b6cb4c3bbf4d
FelixMo42 <felix@tacocat.com>
2018-10-24 19:52:44 -0700

    basic training works!

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from models.LinearModel import LinearModel as Model
 
 model = Model()
 
+model.train()
+
 print(model.test(4))
--- models/LinearModel.py
+++ models/LinearModel.py
@@ -2,17 +2,29 @@ import tensorflow as tf
 
 class LinearModel():
 	name = "LinearModel_V1"
+	learning_rate = .01
 
 	def __init__(self):
-		self.input = tf.Variable(2)
-		self.weight = tf.Variable(4)
-		self.output = tf.multiply(self.input, self.weight)
+		with tf.variable_scope(self.name):
+			self.input = tf.placeholder(tf.float32, (None), name="input")
+			self.weight = tf.Variable(4.0, name="weight")
+			self.output = tf.multiply(self.input, self.weight, name="output")
 
-		self.sess = tf.Session()
-		self.sess.run(tf.global_variables_initializer())
+			self.expected = tf.placeholder(tf.float32, (None), name="expected")
+
+			self.loss = tf.squared_difference(self.expected, self.output)
+			self.optimizer = tf.train.AdagradOptimizer(self.learning_rate)
+			self.trainer = self.optimizer.minimize(self.loss)
+
+			self.sess = tf.Session()
+			self.sess.run(tf.global_variables_initializer())
 
 	def train(self):
-		self.sess.run(self.output, {self.input: x})
+		for i in range(2000):
+			self.sess.run(self.trainer, {
+				self.input: [i],
+				self.expected: [i * 3]
+			})
 
 	def test(self, x):
-		return self.sess.run(self.output, {self.input: x})
+		return self.sess.run(self.output, {self.input: [x]})
