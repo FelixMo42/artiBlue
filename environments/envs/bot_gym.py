6b7442ed10e71e2cb5113df27a41673f601c3f79
FelixMo42 <felix@tacocat.com>
2018-10-28 18:27:07 -0700

    classed DDPG + basic BotGym logic

--- environments/envs/bot_gym.py
+++ environments/envs/bot_gym.py
@@ -1,19 +1,42 @@
+import numpy as np
 import gym
+from gym import spaces
 import pyglet
 
 class BotGym(gym.Env):
-	#metadata = {'render.modes': ['human']}
-
 	_seed =  42
+	window = None
 
 	def __init__(self):
-		pass
+		self.max_speed = 8
+		self.max_torque = 1.
+
+		self.action_num = 1
+		self.observation_num = 4
+
+		self.action_space = spaces.Box(low=-self.max_torque, high=self.max_torque, shape=(self.action_num, ), dtype=np.float32)
+		self.observation_space = spaces.Box(low=-self.max_speed, high=self.max_speed, shape=(self.observation_num, ) , dtype=np.float32)
 
 	def step(self, action):
-	 	pass
+		self.x += action[0]
 
-	def reset(self, sim):
-		pass
+		reward = self.x - self.target_x
+		done = self.x > self.target_x
+
+	 	return self.get_info(), reward, done, {}
+
+	def reset(self):
+		self.x = 10
+		self.y = 10
+
+		self.target_x = 110
+		self.target_y = 10
+
+		return self.get_info()
 
 	def render(self, mode='human', close=False):
-		pass
+		if self.window is None:
+			self.window = pyglet.window.Window(400,400)
+
+	def get_info(self):
+		return np.array([self.x, self.y, self.target_x, self.target_y])
--- trainer/task.py
+++ trainer/task.py
@@ -1,25 +1,5 @@
 from models.DDPG import DDPG
 
-model = DDPG("Pendulum-v0", [2,4,1])
+model = DDPG("BotGym-v0")
 
-'''
-import random
-
-from models.BasicModel import BasicModel
-
-model = BasicModel([2,1])
-
-iterations = 2000
-batch = 100
-for i in range(iterations):
-	i = []
-	e = []
-	for j in range(batch):
-		n1 = random.randint(1,10)
-		n2 = random.randint(1,10)
-		i.append([n1,n2])
-		e.append([n1 * n2])
-	model.train(i, e)
-
-print(model.test([4, 3]))
-'''
+model.train(RENDER=True)
